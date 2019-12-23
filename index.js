0de38c314dd930f8cb1eb0c4ee9bbaa1b1eb5119
unknown <jerson_carranza2@hotmail.com>
2019-12-23 12:57:11 -0500

    v0.1.0

--- index.js
+++ index.js
@@ -5,18 +5,12 @@ var PORT        =  process.env.PORT || 3800;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 var conf    = require('./conf.json');
 var app      = require('./app');
-//var appserver     = new app();
 var server  = http.createServer(app);
-// var uristring =
-//     process.env.MONGOLAB_URI ||
-//     process.env.MONGOHQ_URL ||
-//     'mongodb://localhost:27017/social';
 
 mongoose.Promise = global.Promise;
 
 const uri = "mongodb+srv://"+ conf.db.user+":"+ conf.db.password +"@"+conf.db.host+".azure.mongodb.net/test?retryWrites=true&w=majority";
 
-//const uri= "mongodb+srv://boom:t3tjBtScoliOHf8e@clusterboom-9u5xr.azure.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(uri,{ useNewUrlParser: true })
 .then(() => {
   console.log("La conexion a la base de datos es realizado con exito");
@@ -24,6 +18,3 @@ mongoose.connect(uri,{ useNewUrlParser: true })
       );
 })
 .catch(err => console.log(err));
-
-//mongodb+srv://boom:<password>@clusterboom-9u5xr.azure.mongodb.net/test?retryWrites=true&w=majority
-//MongoNetworkError: failed to connect to server [clusterboom-shard-00-02-9u5xr.azure.mongodb.net:27017] on first connect [MongoNetworkError: connection 4 to clusterboom-shard-00-02-9u5xr.azure.mongodb.net:27017 closed
